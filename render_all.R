c5578f2a85e11bf06b7871d5094884ef950ddb0e
Claire Davis-Reddy <89392858+clairedavis-reddy@users.noreply.github.com>
2026-04-02 17:20:04 +0200

    Delete render_all.R

--- render_all.R
+++
@@ -1,3 +0,0 @@
-rmarkdown::render("notebooks/01_climate_trend_analysis.Rmd")
-rmarkdown::render("notebooks/02_vegetation_phenology.Rmd")
-rmarkdown::render("notebooks/03_long_term_vegetation_trends.Rmd")
